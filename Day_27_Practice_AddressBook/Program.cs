ec3d48ed4fec4a2029bd78485fdea8d23e509c73
mahesh <gangurdemahesh9@gmail.com>
2022-11-15 22:44:23 +0530

    [Mahesh] UC2 add contact detail in program file

--- Day_27_Practice_AddressBook/Program.cs
+++ Day_27_Practice_AddressBook/Program.cs
@@ -8,9 +8,27 @@ namespace Day_27_Practice_AddressBook
             Console.WriteLine("Welcome To Address Book");
             Console.WriteLine("-----------------------");
             Console.WriteLine("-----------------------");
-            Console.WriteLine("---first name------last names---------address------city------state------zip------phone number------email...");
+            Console.WriteLine("--Add Contact Details--");
+
+            Console.WriteLine("First Name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("State:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Zip Code:");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Email Address:");
+            string email = Console.ReadLine();
+
             ContactOperation contactOperation = new ContactOperation();
-            contactOperation.addContact("mahesh", "modi", "maharashtra", "mumbai", "maharashtra", "766452", "9712345678", "abc@gmail.com");
+            contactOperation.addContact(firstName,lastName,address,city,state,zip,phoneNumber,email);
             contactOperation.showList();
         }
     }
