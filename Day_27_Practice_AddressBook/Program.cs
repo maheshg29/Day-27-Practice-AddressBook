e1b598485a336a78b86422fead7ee79522ba6a25
mahesh <gangurdemahesh9@gmail.com>
2022-11-15 22:31:46 +0530

    [Mahesh]add UC1 all files

---
+++ Day_27_Practice_AddressBook/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Day_27_Practice_AddressBook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("---first name------last names---------address------city------state------zip------phone number------email...");
+            ContactOperation contactOperation = new ContactOperation();
+            contactOperation.addContact("mahesh", "modi", "maharashtra", "mumbai", "maharashtra", "766452", "9712345678", "abc@gmail.com");
+            contactOperation.showList();
+        }
+    }
+}
